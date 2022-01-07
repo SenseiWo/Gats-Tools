a0ccfc6c04220dff2ac8195bfd988c8dd658243d
Nitrovesper <79943519+SenseiWo@users.noreply.github.com>
2022-01-07 22:04:02 +0100

    Add files via upload

---
+++ socketTypes/socket.js
@@ -0,0 +1,6 @@
+RF.list[0].socket.send('s,1,3,5'); // Respawn
+RF.list[0].socket.send('k,6,1'); // Fire-shoot
+RF.list[0].socket.send('k,3,1'); // Walk down
+RF.list[0].socket.send('k,2,1'); // Walk up
+RF.list[0].socket.send('k,1,1'); // Walk right
+RF.list[0].socket.send('k,0,1'); // Walk left
